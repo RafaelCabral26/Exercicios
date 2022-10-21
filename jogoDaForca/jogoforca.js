6d13f3d3e0d8fa5f624c7cd87c0c447718782b7b
RafaelCabral <rafaelcabral261992@gmail.com>
2022-10-21 20:59:12 -0300

    forca incomp

--- jogoDaForca/jogoforca.js
+++ jogoDaForca/jogoforca.js
@@ -1,42 +1,68 @@
-    let erros = 6;
-    let acertos = 0;
-    var p = sorteioPalavras();
-    criarLinhas(p);
-    document.getElementById("btn").addEventListener("click", function(){checarLetra(p, erros, acertos)});
 
+    var p = sorteioPalavras(); 
+    criarLinhas(p);
+    document.getElementById("btn").addEventListener("click", function(){controllerForca(p,acertos, erros)});
+    
+    var arrContador = [];
+    var erros = 6;
+    var acertos = 0;
+    
+    function controllerForca(p) {
+        let letra = document.getElementById("letra").value;
+        contadorErrosAcertos(p,letra, acertos, erros, arrContador)
+        checarVitoriaDerrota(acertos,erros);
+        contadorLetra(p,letra,arrContador);
+    }
 
 function sorteioPalavras() {
     let palavras  = ["despertador", "entorpecido", "colher", "arado", "sucesso", "xilofone", "maiô", "tomate", "zeus", "senhoria"];
     let p = Math.floor(Math.random() * palavras.length);
-    let palavra = palavras[p].toString();
+    let palavra = palavras[p];
     console.log(palavra);
     return palavra;
 }
 function criarLinhas(p) {
-    for(i = 0; i < p.length;i++) {
+    for(let i = 0; i < p.length;i++) {
         document.getElementById("palavra").innerHTML += "<div class='letras'> __ </div>";
+    };
+};
+//Contar Num de Erros e Acertos
+function contadorErrosAcertos(pa,letra, acertos, erros, arrContador) {
+    for(let i = 0;i < pa.length; i++) {
+        if(letra == pa[i] && arrContador.indexOf(letra) == -1) {
+            acertos++
+        }
+    }
+    for(let j = 0;j <p.length;j++)
+        if(letra != pa[i]) {
+        erros--
+    }
+}
+//Checar Vitória 
+
+function checarVitoriaDerrota(acertos, erros) {
+    if(erros == 0) {
+        //Inserir mudança na img forca e reload da página
+        return alert("Perdeu!!!");
+    }else if(acertos == p.length) {
+        //Inserir mudança na img forca e reload da página
+        return alert("Vitória");
     };
 };
 
 //Checar Letra 
 
-function checarLetra(pa, erros, acertos) {
-    let letra = document.getElementById("letra").value;
-    let underlines = document.getElementById("palavra").children;
-    let arrUnderlines = [...underlines];
-    for(i = 0; i < pa.length; i++) {
-        if(letra == pa[i]) {
-            arrUnderlines[i].innerHTML = letra;
-            acertos++
-            
-            }
-        }; 
-    let error = document.getElementById("erros").innerHTML;
-    let erro = document.getElementById("erros").children
-    let arrErro = [...erro];
-    for(j = 0; j < pa.length; j++) {
-        if(letra != pa[j] && arrErro.includes(letra) == false) {
-        arrErro += "<div class='erro'>" + letra +  "</div>"
-        }
-    }
+
+
+function contadorLetra(pa, letra, arrContador) {
+    for(let i = 0; i < pa.length; i++) {
+        if(letra == pa[i] && arrContador.indexOf(letra) == -1) {
+            arrContador.push(letra);
+                
+        };
+    };  
 };
+        
+        
+    
+
